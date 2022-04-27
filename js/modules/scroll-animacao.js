80cd3a6f9cb27ddb3041a452639a549934a27b1c
Eduardo Cezar <ecezar07@gmail.com>
2022-04-27 12:28:14 -0300

    numeros

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -1,17 +1,18 @@
 export default function initAnimacaoScroll() {
   const sections = document.querySelectorAll('[data-anime="scroll"]');
-  if(sections.length) {
+  if (sections.length) {
     const windowMetade = window.innerHeight * 0.6;
 
     function animaScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
           section.classList.add('ativo');
-        else 
+        } else if (section.classList.contains('ativo')) {
           section.classList.remove('ativo');
-      })
+        }
+      });
     }
 
     animaScroll();
